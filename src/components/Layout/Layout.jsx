fa29f5d0cd3f25a46633a56ae7fd7089fad06ad4
quangdm <qduongminh3@gmail.com>
2023-02-09 23:32:56 +0700

    add TopPromotion

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import "./Layout.css";
 const Layout = ({ children }) => {
   return (
-    <div className="layout">
+    <div class="main-body">
+      <div class="countdownTopPromotion">
+        <a href="https://evadeeva.com.vn/collections/end-of-season-sale-50-70-eoss03">
+          <img
+            class="hidden-xs"
+            src="//theme.hstatic.net/200000000133/1000569834/14/banner_countdown_top_desktop.jpg?v=6313"
+            alt="sale"
+          />
+        </a>
+      </div>
       <Header />
-      <main className="main-container">{children}</main>
+      <div class="clear-fix"></div>
+      <main class="main-container">{children}</main>
       <footer>
         <Footer />
       </footer>
