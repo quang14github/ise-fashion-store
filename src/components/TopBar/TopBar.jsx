03d60728442faa143c40d355739252a6ee802419
quangdm <qduongminh3@gmail.com>
2023-02-09 23:34:50 +0700

    create TopBar

---
+++ src/components/TopBar/TopBar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./TopBar.css";
+const TopBar = () => {
+  return (
+    <div class="topbar">
+      <div class="padding-lr-0 col-xs-12 col-sm-8 col-md-8 col-lg6">
+        <div class="innerTopbar">
+          <span class="hight-space">-</span>
+          <span>
+            <a
+              href="tel:18001731"
+              title="Mọi thắc mắc hoặc phản hồi dịch vụ, xin liên hệ hotline CSKH:  18001731 "
+            >
+              Mọi thắc mắc hoặc phản hồi dịch vụ, xin liên hệ hotline CSKH:{" "}
+              <b>18001731 </b>
+            </a>
+          </span>
+          <span>-</span>
+          <span>
+            <a href="tel:18001732." title="Mua hàng Online: 18001732.">
+              Mua hàng Online: <b>18001732.</b>
+            </a>
+          </span>
+        </div>
+      </div>
+      <div class="padding-lr-0 col-xs-12 col-sm-4 col-md-4 col-lg6">
+        <div class="innerTopbar text-right"></div>
+      </div>
+    </div>
+  );
+};
+
+export default TopBar;
