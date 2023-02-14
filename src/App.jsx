1026111832b936e7212b2bdd58ba5dcf1094275a
bun beos <trungkienttmh@gmail.com>
2023-02-15 01:40:53 +0700

    update app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
-import Home from "./pages/Home";
+import Home from "./pages/Home/Home";
 import LogIn from "./pages/LogIn/LogIn";
 import Register from "./pages/Register/Register";
 const App = () => {
