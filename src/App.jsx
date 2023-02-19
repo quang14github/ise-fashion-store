14b92d89e8aa829aaea1cb95bd1b1eccae401bda
bun beos <trungkienttmh@gmail.com>
2023-02-19 16:21:22 +0700

    update Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
-import Account from "./pages/Accout/Account";
 import Home from "./pages/Home/Home";
 import LogIn from "./pages/LogIn/LogIn";
 import Register from "./pages/Register/Register";
+import Account from "./pages/Account/Account";
+import Cart from "./pages/Cart/Cart";
 const App = () => {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/account/login" element={<LogIn />} />
-        <Route path="/account/register" element={<Register />} />
-        <Route path="/account" element={<Account />} />
-
-      </Routes>
-    </Layout>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/account/login" element={<LogIn />} />
+          <Route path="/account/register" element={<Register />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 };
 
 export default App;
+
