62ac09824bdb2d2976f5278dc96e9718e05cdcab
quangdm <qduongminh3@gmail.com>
2023-02-19 15:41:41 +0700

    update Layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -4,22 +4,20 @@ import Footer from "../Footer/Footer";
 import "./Layout.css";
 const Layout = ({ children }) => {
   return (
-    <div class="main-body">
-      <div class="countdownTopPromotion">
+    <div className="main-body">
+      <div className="countdownTopPromotion">
         <a href="https://evadeeva.com.vn/collections/end-of-season-sale-50-70-eoss03">
           <img
-            class="hidden-xs"
+            className="hidden-xs"
             src="//theme.hstatic.net/200000000133/1000569834/14/banner_countdown_top_desktop.jpg?v=6313"
             alt="sale"
           />
         </a>
       </div>
       <Header />
-      <div class="clear-fix"></div>
-      <main class="main-container">{children}</main>
-      <footer>
-        <Footer />
-      </footer>
+      <div className="clear-fix"></div>
+      <main className="main-container">{children}</main>
+      <Footer />
     </div>
   );
 };
