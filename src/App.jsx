6c2226c724fd1928af84241a4966be69a901f5d2
quangdm <qduongminh3@gmail.com>
2023-02-19 12:36:23 +0700

    Add Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
-import Home from "./pages/Home";
+import Home from "./pages/Home/Home";
+import Collection from "./pages/Collection/Collection";
 const App = () => {
   return (
-    <Layout>
-      <Home />
-    </Layout>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/collections" element={<Collection />}></Route>
+        </Routes>
+      </Layout>
+    </Router>
   );
 };
 
