6bc5ab4986515b13d2d67cb5a62145d8b9771939
quangdm <qduongminh3@gmail.com>
2023-02-19 15:41:15 +0700

    update Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,43 +1,44 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import TopBar from "../TopBar/TopBar";
 import "./Header.css";
 const Header = () => {
   return (
-    <header id="header" class="clearfix">
+    <header id="header" className="clearfix">
       <TopBar />
-      <div class="main-header hidden-xs">
-        <div class="padding-lr-0 col-xs-12 col-sm-3 col-md-3 col-lg-2">
+      <div className="main-header hidden-xs">
+        <div className="padding-lr-0 col-xs-12 col-sm-3 col-md-3 col-lg-2">
           <div
-            class="logo-main"
-            itemscope=""
-            itemtype="http://schema.org/Organization"
+            className="logo-main"
+            itemScope=""
+            itemType="http://schema.org/Organization"
           >
-            <a href="https://evadeeva.com.vn" title="Logo" itemprop="url">
+            <NavLink to="/">
               <img
                 src="//file.hstatic.net/1000358207/file/logo_eva.svg"
                 alt="Eva De Eva"
-                class="img-responsive logoimg"
-                itemprop="logo"
+                className="img-responsive logoimg"
+                itemProp="logo"
               />
-            </a>
+            </NavLink>
           </div>
         </div>
         <Navbar />
-        <div class="padding-lr-0 col-xs-12 col-sm-9 col-md-9 col-lg-3">
-          <div class="rightHeader">
-            <div class="bagHeader">
+        <div className="padding-lr-0 col-xs-12 col-sm-9 col-md-9 col-lg-3">
+          <div className="rightHeader">
+            <div className="bagHeader">
               <span>
                 <a href="/cart" title="Giỏ hàng">
                   <img
                     src="//theme.hstatic.net/200000000133/1000569834/14/bagIcon2.png?v=6303"
                     alt="Giỏ hàng"
                   />
-                  <span class="countCart">0</span>
+                  <span className="countCart">0</span>
                 </a>
               </span>
             </div>
-            <div class="accountHeader">
+            <div className="accountHeader">
               <a href="/account/login" title="Tài khoản">
                 <img
                   src="//theme.hstatic.net/200000000133/1000569834/14/accountIcon.png?v=6303"
@@ -45,22 +46,22 @@ const Header = () => {
                 />
               </a>
             </div>
-            <div class="searchHeader">
-              <form action="/search" class="searchDesktop">
+            <div className="searchHeader">
+              <form action="/search" className="searchDesktop">
                 <input type="hidden" name="type" value="product" />
                 <input
                   required=""
                   type="text"
                   name="q"
                   id="searchInputDesktop"
-                  class="searchInput"
+                  className="searchInput"
                   placeholder="Tìm sản phẩm..."
                   value=""
                 />
-                <input type="submit" class="searchButton" value="" />
+                <input type="submit" className="searchButton" value="" />
               </form>
-              <div id="resultSearchDesktop" class="resultDesktop">
-                <div class="innerResultDesktop"></div>
+              <div id="resultSearchDesktop" className="resultDesktop">
+                <div className="innerResultDesktop"></div>
               </div>
             </div>
           </div>
