0c279b01f4d26ca037684b42af9a19a9f8a8d594
quangdm <qduongminh3@gmail.com>
2023-02-19 15:40:59 +0700

    update Home

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,255 @@
+import React from "react";
+import "./Home.css";
+const Home = () => {
+  return (
+    <>
+      <h1 className="hidden">Eva De Eva</h1>
+      <div id="home-slider">
+        <a href="https://evadeeva.com.vn/collections/hang-moi-ve-2022">
+          <img
+            title="HÀNG MỚI VỀ"
+            alt="HÀNG MỚI VỀ"
+            src="//theme.hstatic.net/200000000133/1000569834/14/slideshow_1.jpg?v=6336"
+          />
+        </a>
+      </div>
+      <section
+        id="home-3Banner"
+        className="animation-eva clear-fix margin-tb-50 active"
+      >
+        <div className="padding-rl-40">
+          <div className="inner3Banner styleAnimteBanner">
+            <div className="col-xs-12 col-sm-4 site-animation">
+              <div className="animateBanner marginTopbanner">
+                <a
+                  href="https://evadeeva.com.vn/collections/hang-moi-ve-2022"
+                  title="NEW ARRIVALS"
+                >
+                  <img
+                    src="//theme.hstatic.net/200000000133/1000569834/14/home3banner_1.jpg?v=6336"
+                    alt="NEW ARRIVALS"
+                  />
+                </a>
+              </div>
+            </div>
+            <div className="col-xs-12 col-sm-4 site-animation">
+              <div className="animateBanner">
+                <a
+                  href="https://evadeeva.com.vn/collections/hang-moi-ve-2022"
+                  title="NEW ARRIVALS"
+                >
+                  <img
+                    src="//theme.hstatic.net/200000000133/1000569834/14/home3banner_2.jpg?v=6336"
+                    alt="NEW ARRIVALS"
+                  />
+                </a>
+              </div>
+            </div>
+            <div className="col-xs-12 col-sm-4 site-animation">
+              <div className="animateBanner marginTopbanner">
+                <a
+                  href="https://evadeeva.com.vn/collections/hang-moi-ve-2022"
+                  title="NEW ARRIVALS"
+                >
+                  <img
+                    src="//theme.hstatic.net/200000000133/1000569834/14/home3banner_3.jpg?v=6336"
+                    alt="NEW ARRIVALS"
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="home-aboutUs" className="animation-eva active">
+        <div className="innerHomeAboutUs">
+          <div className="padding-lr-0 col-xs-12 col-sm-7">
+            <div className="leftAboutUs site-animation">
+              <div className="imageAboutUs">
+                <a
+                  href="https://evadeeva.com.vn/collections/hang-moi-ve-2022"
+                  title="FEMME FW'22 Campaign"
+                >
+                  <img
+                    src="//theme.hstatic.net/200000000133/1000569834/14/home_aboutus.jpg?v=6336"
+                    alt="FEMME FW'22 Campaign"
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="padding-lr-0 col-xs-12 col-sm-5">
+            <div className="rightAboutUs site-animation">
+              <div className="sectionTitleAboutus">
+                <h2>
+                  <a
+                    href="https://evadeeva.com.vn/collections/hang-moi-ve-2022"
+                    title="LOVE MORE"
+                  >
+                    LOVE MORE
+                  </a>
+                </h2>
+              </div>
+              <div className="sectionContentAboutus">
+                <p style={{ textAlign: "justify" }}>
+                  Tình yêu là gì? Là cái ôm siết chặt, cái nắm tay ấm áp hay một
+                  nụ hôn khẽ chạm cũng đủ làm tim bồi hồi thổn thức. Tình yêu
+                  dắt ta qua những cung bậc của cảm xúc, có lúc lắng đọng, có
+                  lúc lại cuồng nhiệt đắm say. Trước những bấp bênh trong cuộc
+                  đời, tình yêu còn là liều thuốc kì diệu để chữa lành cho tâm
+                  hồn đang còn vết xước. Với 𝐋𝐎𝐕𝐄 𝐌𝐎𝐑𝐄, Eva de Eva tái hiện hình
+                  ảnh cô gái đang khoác lên mình những thiết kế bay bổng và say
+                  mê tận hưởng cảm xúc lứa đôi ngọt ngào.{" "}
+                </p>
+                <div className="btnCenter">
+                  <a
+                    className="btnAllSite viewMoreAboutUS"
+                    href="https://evadeeva.com.vn/collections/hang-moi-ve-2022"
+                    title="Tìm hiểu thêm"
+                  >
+                    Tìm hiểu thêm
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="home-bannerCenter">
+        <div
+          className="innerHomeBannerCenter"
+          data-link="https://evadeeva.com.vn/pages/love-more"
+        ></div>
+      </section>
+      <section id="home-news" className="animation-eva active">
+        <div className="padding-rl-40">
+          <div className="container contentHomeWidth">
+            <div className="innerHomeNews">
+              <div className="sectionTitle site-animation">
+                <h2 className="heading-title">
+                  <a href="/blogs/tin-tuc" title="TIN TỨC EVA DE EVA">
+                    TIN TỨC EVA DE EVA
+                  </a>
+
+                  <p className="viewMore">
+                    <a href="/blogs/tin-tuc">Xem tất cả</a>
+                  </p>
+                </h2>
+              </div>
+            </div>
+          </div>{" "}
+        </div>
+      </section>
+
+      <div className="mg-10  home-blogs innerHomeCollection animation-eva active">
+        <div className="container ">
+          <div
+            className="col-md-6 col-sm-12 col-xs-12 owl-item site-animation"
+            style={{ marginBottom: "20px" }}
+          >
+            <div className="item">
+              <div className="new-item">
+                <div className="col-xs-6 no-padding-r home-blog-content-left">
+                  <div className="new-img banner-effect">
+                    <a
+                      href="/blogs/tin-tuc/new-collection-afterglow"
+                      title="[NEW COLLECTION] - AFTERGLOW"
+                    >
+                      <img
+                        src="//file.hstatic.net/200000000133/article/22acve044x_9056c1bc2a4c4511b4d448a566e0be73_grande.jpg"
+                        alt="[NEW COLLECTION] - AFTERGLOW"
+                      />
+                    </a>
+                  </div>
+                </div>
+                <div className="col-xs-6 no-padding-l home-blog-content-right">
+                  <div
+                    className="home-blog-content-right-div"
+                    style={{ textAlign: "justify" }}
+                  >
+                    <h3 className="new-name">
+                      <a
+                        href="/blogs/tin-tuc/new-collection-afterglow"
+                        title="[NEW COLLECTION] - AFTERGLOW"
+                      >
+                        [NEW COLLECTION] - AFTERGLOW
+                      </a>
+                    </h3>
+                    <p className="new-description">
+                      Ánh chiều hoàng hôn đầy chất thơ luôn mang đến cho những
+                      nhà thiết kế thật nhiều cảm hứng – một vẻ đẹp rực rỡ nhưng
+                      phảng phất nhiều suy tư, một nét trầm lắng trong tâm hồn
+                      của những người phụ nữ trưởng thành.Đến với bộ sưu tập
+                      𝐀𝐅𝐓𝐄𝐑𝐆𝐋𝐎𝐖,...
+                      <br />
+                      <a
+                        className="home_blog_view_more example_e"
+                        href="/blogs/tin-tuc/new-collection-afterglow"
+                      >
+                        Xem thêm <i className="fa fa-angle-double-right" />
+                      </a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div
+            className="col-md-6 col-sm-12 col-xs-12 owl-item site-animation"
+            style={{ marginBottom: "20px" }}
+          >
+            <div className="item">
+              <div className="new-item">
+                <div className="col-xs-6 no-padding-r home-blog-content-left">
+                  <div className="new-img banner-effect">
+                    <a
+                      href="/blogs/tin-tuc/femme-danh-dau-hanh-trinh-15-nam-thoi-trang-cua-eva-de-eva-23"
+                      title="FEMME đánh dấu hành trình 15 năm thời trang của Eva de Eva"
+                    >
+                      <img
+                        src="//file.hstatic.net/200000000133/article/308634448_5535283309925636_4012878731319835600_n_978b630c561b40b5a391f13c32e3e51d_grande.jpg"
+                        alt="FEMME đánh dấu hành trình 15 năm thời trang của Eva de Eva"
+                      />
+                    </a>
+                  </div>
+                </div>
+                <div className="col-xs-6 no-padding-l home-blog-content-right">
+                  <div
+                    className="home-blog-content-right-div"
+                    style={{ textAlign: "justify" }}
+                  >
+                    <h3 className="new-name">
+                      <a
+                        href="/blogs/tin-tuc/femme-danh-dau-hanh-trinh-15-nam-thoi-trang-cua-eva-de-eva-23"
+                        title="FEMME đánh dấu hành trình 15 năm thời trang của Eva de Eva"
+                      >
+                        FEMME đánh dấu hành trình 15 năm thời trang của Eva de
+                        Eva
+                      </a>
+                    </h3>
+                    <p className="new-description">
+                      𝐅𝐄𝐌𝐌𝐄 đánh dấu hành trình 15 năm thời trang của Eva de
+                      Eva, là một điểm chạm rực rỡ gói trọn tâm huyết mà chúng
+                      tôi ấp ủ. BST gợi tả hình ảnh người phụ nữ độc lập mang vẻ
+                      đẹp sang trọng và thần thái. Một định nghĩa mới...
+                      <br />
+                      <a
+                        className="home_blog_view_more example_e"
+                        href="/blogs/tin-tuc/femme-danh-dau-hanh-trinh-15-nam-thoi-trang-cua-eva-de-eva-23"
+                      >
+                        Xem thêm <i className="fa fa-angle-double-right" />
+                      </a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
