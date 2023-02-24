ccd301551340701ba09769d8e63a8417acc43cad
quangdm <qduongminh3@gmail.com>
2023-02-24 13:09:04 +0700

    create CheckError util

---
+++ src/utils/CheckError.js
@@ -0,0 +1,7 @@
+export function CheckError(response) {
+  if (response.status >= 200 && response.status <= 299) {
+    return response.json();
+  } else {
+    throw Error(response.statusText);
+  }
+}
