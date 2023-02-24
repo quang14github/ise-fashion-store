219a62aae80de233653dbf7b804be71f4a441458
quangdm <qduongminh3@gmail.com>
2023-02-24 15:11:02 +0700

    add product detail and 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,25 @@ import Register from "./pages/Register/Register";
 import Account from "./pages/Account/Account";
 import Cart from "./pages/Cart/Cart";
 import Collection from "./pages/Collection/Collection";
+import Product from "./pages/Product/Product";
+import NotFound from "./pages/NotFound/NotFound";
 const App = () => {
   return (
     <Router>
-    <Layout>
-      <Routes>
-        <Route exact path="/" element={<Home />}></Route>
-        <Route exact path="/collections" element={<Collection />}></Route>
-        <Route path="/account/login" element={<LogIn />}></Route>
-        <Route path="/account/register" element={<Register />}></Route>
-        <Route path="/account" element={<Account />}></Route>
-        <Route path="/cart" element={<Cart />}></Route>
-      </Routes>
-    </Layout>
-  </Router>
+      <Layout>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/collections" element={<Collection />}></Route>
+          <Route path="/collections/:productId" element={<Product />}></Route>
+          <Route path="/account/login" element={<LogIn />}></Route>
+          <Route path="/account/register" element={<Register />}></Route>
+          <Route path="/account" element={<Account />}></Route>
+          <Route path="/cart" element={<Cart />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </Layout>
+    </Router>
   );
 };
 
 export default App;
-
